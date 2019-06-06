d2904151f47c66fda526373e89cdaab7da0fe6a4
annicole <annicole_rosa@outlook.com>
2019-06-06 09:56:54 -0500

    add cors dependency

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var express = require('express'),
   usuario = require('./api/models/user'), //created model loading here
   posicion = require('./api/models/posicion'), //created model loading here
   bodyParser = require('body-parser');
-  
+ 
+ var cors = require('cors');
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/quiniela', { useNewUrlParser: true }); 
@@ -13,7 +15,8 @@ mongoose.connect('mongodb://localhost/quiniela', { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors());
+ 
 
 var routes = require('./api/routes/user'); //importing route
 var posicionRoute = require('./api/routes/posicion');
